94b2f092610dba9163285b3e8843514f84110b6b
Satyapriya Barik <satya.barik27@gmail.com>
2026-03-08 13:36:37 +0530

    api url changes

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:4000"
+  uri: "http://13.234.136.192:4000/graphql"
+  // "http://localhost:4000"
 });
 
 export const client = new ApolloClient({
